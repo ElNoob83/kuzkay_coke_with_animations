9723744323534090aab49431f519bf4765f54197
ElNoob83 <61382605+ElNoob83@users.noreply.github.com>
2021-05-04 23:42:19 -0300

    Delete Kuzkay_Coke directory

--- Kuzkay_Coke/__resource.lua
+++
@@ -1,7 +0,0 @@
-print("Kuzkay_Coke has successfully started. Enjoy!")
-
-client_scripts {
-  'config.lua',
-  'client.lua'
-}
-server_script "server.lua"
--- Kuzkay_Coke/config.lua
+++
@@ -1,6 +0,0 @@
-Config = {}
-
-Config.PickupBlip = {x = 2676.78  , y = -854.67,z = 27.34}
-Config.Processing = {x = 2434.2  , y = 4968.6, z =41.3}      
-
-
